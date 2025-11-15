441353cc1ee613f42265ab61068547dd8b0e9148
sunthans <sathyasunthan098@gmail.com>
2025-11-15 19:20:36 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java Application running in Jenkins!");
+    }
+}
